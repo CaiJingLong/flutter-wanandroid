cd64c47e5ff58d3f237b29300e694b9392a207a6
cjl-macbook pro <cjl_spy@163.com>
2018-04-30 22:18:22 +0800

    like 相关

--- lib/pages/LikePage.dart
+++ lib/pages/LikePage.dart
@@ -7,17 +7,20 @@ import 'package:flutter_wanandroid/helper/UserInfoHelper.dart';
 import 'package:flutter_wanandroid/pages/login/LoginPage.dart';
 
 abstract class LikePage extends UserInfoHelper {
-
   Future<bool> _like(HomeData data) async {
     return true;
   }
 
   Future<bool> like(BuildContext context, HomeData data) async {
     var islogin = await isLogin();
-    if (islogin) {
+    if (!islogin) {
       Navigator.of(context).push(new MaterialPageRoute(builder: (ctx) {
         return new LoginPage();
-      }));
+      })).then((result) {
+        if (result) {
+          _like(data);
+        }
+      });
       return true;
     }
     return false;
